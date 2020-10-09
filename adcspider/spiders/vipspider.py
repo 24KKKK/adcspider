ed41fa85e39c31c6e55bde6f9a799787b2fd9ac1
24k <2991736073@qq.com>
2020-10-09 21:20:37 +0800

    完善爬取过程

--- adcspider/spiders/vipspider.py
+++ adcspider/spiders/vipspider.py
@@ -7,33 +7,30 @@ class VipspiderSpider(scrapy.Spider):
     name = 'vipspider'
     allowed_domains = ['yglz.tousu.hebnews.cn/']
     start_urls = ['http://yglz.tousu.hebnews.cn/shss-1.html']
-    custom_settings = {
-        'LOG_LEVEL': 'ERROR'
-    }
-    pageCount = 1
+    # custom_settings = {
+    #     'LOG_LEVEL': 'ERROR'
+    # }
+    pageNo = 1
+    pageCount = 20  # 一共要爬取得页数
 
     def parse(self, response):
-        startTime = datetime.datetime.now()
-        print('----------------------')
         item = AdcspiderItem()
         for i in range(1, 21):
             for each in response.xpath('//*[@id="divList"]/div[' + str(i) + ']'):
-                newsType = each.xpath('./span[2]/p/text()').extract_first()
+                newsType = each.xpath('./span[2]/p/text()').extract_first()[1:-1]
                 newsTitle = each.xpath('./span[3]/p/a/text()').extract_first()
-                newsTime = each.xpath('./span[4]/p/text()').extract_first()
+                newsTime = each.xpath('./span[4]/p/text()').extract_first()[1:-1]
                 if '石家庄' in newsTitle:
-                    print('分类:', newsType, '标题:', newsTitle, '时间:', newsTime)
+                    print('类型:' + newsType + ' 时间:' + newsTime + ' 标题:' + newsTitle, end='\n\n')
                     item['newsType'] = newsType
                     item['newsTitle'] = newsTitle
                     item['newsTime'] = newsTime
                     yield item
-        self.pageCount = self.pageCount + 1
-        if self.pageCount < 20:
-            next_url = 'http://yglz.tousu.hebnews.cn/shss-' + str(self.pageCount) + '.html'
-            print(next_url)
+        self.pageNo = self.pageNo + 1
+        if self.pageNo <= self.pageCount:
+            next_url = 'http://yglz.tousu.hebnews.cn/shss-' + str(self.pageNo) + '.html'
+            print('第 ' + str(self.pageNo) + ' 页：' + next_url)
             yield scrapy.Request(url=next_url, callback=self.parse, dont_filter=True)
-        endTime = datetime.datetime.now()
-        print('运行时间：', (endTime - startTime).seconds)
         # next_no = response.xpath('//*[@id="divList"]/div[21]/a[1]/@onclick').extract()
         # arr = next_no[0].split('|')
         # pageNo = arr[2][0:1]
--- main.py
+++ main.py
@@ -7,7 +7,4 @@ import os  # 用来获取路径的模块
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # execute(['scrapy', 'crawl', 'vipspider']) # 显示日志
-startTime = datetime.datetime.now()
 cmdline.execute('scrapy crawl vipspider --nolog'.split())  # 运行爬虫时，不显示scrapy日志
-endTime = datetime.datetime.now()
-print('运行时间：', (endTime - startTime).seconds)
